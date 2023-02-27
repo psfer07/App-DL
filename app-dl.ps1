f3b44b2f6508165df09c8bb02f1a44277adf8f5a
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-02-27 20:37:42 +0100

    Last stable release

--- app-dl.ps1
+++ app-dl.ps1
@@ -1,4 +1,5 @@
-function Write-Main{
+Clear-Host
+function Write-Main() {
   [CmdletBinding()]
   param (
       [String] $Text = "Blank"
@@ -7,92 +8,56 @@ function Write-Main{
 
   $loop = $Text.Length
 
-  $center = for ($i = 1; $i -le $loop; $i++){ ("═") }
+  $center = for ($i = 1; $i -le $loop; $i++){ ("=") }
 
   Write-Host "`n<$center>" -ForegroundColor Blue
   Write-Host "   $Text" -ForegroundColor White -BackgroundColor Black
   Write-Host "<$center>" -ForegroundColor Blue -BackgroundColor Black
 }
-function Write-Secondary{
+function Write-Secondary() {
   [CmdletBinding()]
   param (
       [String] $Text = "Blank"
   )
 
-  Write-Host "`n<══════════╣" -NoNewline -ForegroundColor Green -BackgroundColor Black
+  Write-Host "`n<==========[" -NoNewline -ForegroundColor Green -BackgroundColor Black
   Write-Host " $Text " -NoNewline -ForegroundColor White -BackgroundColor Black
-  Write-Host "╠══════════>`n" -ForegroundColor Green -BackgroundColor Black
+  Write-Host "]==========>`n" -ForegroundColor Green -BackgroundColor Black
 }
-function Write-Point{
+function Write-Point() {
   [CmdletBinding()]
   param (
       [String] $Text = "Blank"
   )
 
-  Write-Host "══╣ " -NoNewline -ForegroundColor Green -BackgroundColor Black
+  Write-Host "==[ " -NoNewline -ForegroundColor Green -BackgroundColor Black
   Write-Host "$Text" -ForegroundColor White -BackgroundColor Black
 }
-function Restart{
-if (Test-Path "$path\$program") {
-  Write-Point "1. Restart App-dl"
-  Write-Point "2. Leave powershell"
-  $restart = Read-Host "This program or the zip file of it is currently allocated in this path, so select the corresponding number if you want to start again or leaving this session"
-if ($restart -eq 1) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""; exit }
-elseif ($restart -ne 1) {
-  Write-Main "Closing this terminal..."
-  Start-Sleep -Milliseconds 500
-  exit
-}}
-}
-function Select-app{
-Clear-Host
-Write-Main "Aviable apps"
-foreach ($i in 0..($filteredApps.Count - 1)) {
-  $app = $filteredApps[$i]
-  $n = $i + 1
-  Write-Secondary "$n. $($app.Name) - Size: $($app.Size)"
-}
-$pkg_number = Read-Host `n"Write the number of the app you want to get $help"
-if ($pkg_number = "h","H"){Restart}
-}
-function Select-path{
-  Clear-Host
-  Write-Main "$program selected"
-  
-  Write-Point "1. Saves it inside of Desktop"
-  Write-Point "2. Saves it inside of Documents"
-  Write-Point "3. Saves it inside of Downloads"
-  Write-Point "4. Save it inside of C:"
-  Write-Point "5. Saves it inside of Program Files"
-  Write-Point "6. Save it inside of the user profile"
-  $path = Read-Host "`nChoose a number $help"
-  
-  switch ($path) {
-    1         { $path = "$Env:USERPROFILE\Desktop"; break }
-    2         { $path = "$Env:USERPROFILE\Documents"; break }
-    3         { $path = "$Env:USERPROFILE\Downloads"; break }
-    4         { $path = $Env:SystemDrive; break }
-    5         { $path = $Env:ProgramFiles; break }
-    6         { $path = $Env:HOMEPATH; break }
-    "h""H"    { Select-path}
-    default   { Write-Host "Invalid input. Using default path: $Env:USERPROFILE"; $path = $Env:USERPROFILE }
-  }
-  Write-Point "Selected path: $path"
-}
+function Write-Warning() {
+  [CmdletBinding()]
+  param (
+      [String] $Text = "Blank"
+  )
 
 
-########################
-# Initialize variables #
-########################
+  $loop = $Text.Length
 
+  $center = for ($i = 1; $i -le $loop; $i++){ ("=") }
+
+  Write-Host "`n<$center>" -ForegroundColor Red
+  Write-Host "   $Text" -ForegroundColor White -BackgroundColor Black
+  Write-Host "<$center>" -ForegroundColor Red -BackgroundColor Black
+}
+
+#Initialize variables
 $json = Get-Content ".\apps.json" -Raw | ConvertFrom-Json
 $nameArray = $json.psobject.Properties.Name
 $propMapping = @{}
 $filteredApps = @()
-$help = "(or type 'h' for help)"
-$help_msg = "You can press 'R' to restart the app"
 
+Clear-Host
 
+#Assigns the JSON's properties into Powershell objects
 foreach ($i in 0..($nameArray.Count - 1)) {
   $name = $nameArray[$i]
   $app = $json.$name
@@ -103,28 +68,70 @@ foreach ($i in 0..($nameArray.Count - 1)) {
   $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; URL = $url; Exe = $exe;Size = $size}
 }
 
-
-#Clear-Host
+#List every app valid in the JSON file
 Write-Main "Aviable apps"
 foreach ($i in 0..($filteredApps.Count - 1)) {
   $app = $filteredApps[$i]
   $n = $i + 1
   Write-Secondary "$n. $($app.Name) - Size: $($app.Size)"
 }
-$pkg_number = Read-Host `n"Write the number of the app you want to get" $help
-if ($pkg_number = "h" -or "H"){Write-Main $help_msg; break}
-
+$pkg_number = Read-Host `n"Write the number of the app you want to get"
 $program = $nameArray[$pkg_number - 1]
 
-Restart
+Clear-Host
+Write-Main "$program selected"
 
+# Prints off all the aviable paths to ssave the package
+Write-Point "0. Goes back to change the app"
+Write-Point "1. Saves it inside of Desktop"
+Write-Point "2. Saves it inside of Documents"
+Write-Point "3. Saves it inside of Downloads"
+Write-Point "4. Save it inside of C:"
+Write-Point "5. Saves it inside of Program Files"
+Write-Point "6. Save it inside of the user profile"
+$path = Read-Host "`nChoose a number $help"
+
+switch ($path) {
+  0         {  ;break}
+  1         { $path = "$Env:USERPROFILE\Desktop"; break }
+  2         { $path = "$Env:USERPROFILE\Documents"; break }
+  3         { $path = "$Env:USERPROFILE\Downloads"; break }
+  4         { $path = $Env:SystemDrive; break }
+  5         { $path = $Env:ProgramFiles; break }
+  6         { $path = $Env:HOMEPATH; break }
+  default   { Write-Host "Invalid input. Using default path: $Env:USERPROFILE"; $path = $Env:USERPROFILE }
+}
+Write-Main "Selected path: $path"
+
+#Checks if the program is installed or uncompressed in the selected folder
+if (Test-Path "$path\$program") {
+  Write-Host `n
+  Write-Point "1. Restart App-dl"
+  Write-Point "2. Leave powershell"
+  $restart = Read-Host "This program or the zip file of it is currently allocated in this path, so select the corresponding number if you want to start again or leaving this session"
+if ($restart -eq 1) { Start-Process powershell.exe "-WindowStyle Maximized -File `"$PSCommandPath`""; break}
+elseif ($launch -ne 1) {
+  Write-Main "Closing this terminal..."
+  Start-Sleep -Milliseconds 500
+  exit
+}}
+
+# Assign specific variables to download and save the package
 $selectedApp = $filteredApps | Where-Object {$_.Name -eq $program}
 $url = $selectedApp.URL
 $out_file = (Split-Path $url -Leaf) -split "/" | Select-Object -Last 1
 
+
+# Downloads the app package
 Write-Main "App to download: $program..."; Pause
 Invoke-WebRequest -URI $url -OutFile "$path\$out_file"
+if ($?) {
+    Write-Secondary "File downloaded successfully"
+} else {
+    Write-Warning "An error occurred while downloading the file"
+}
 
+# Extraction or installation of the app depending on its extension
 if ($out_file -like "*.zip"){$extract = Read-Host "Do you want to unzip the package?(y/n)"
 if ($extract -eq "y" -or $extract -eq "Y"){
   try {
@@ -133,7 +140,7 @@ if ($extract -eq "y" -or $extract -eq "Y"){
     Start-Sleep -Seconds 2
   }
   catch {
-    Write-Host "Failed to extract package. Error: $($_.Exception.Message)"; Pause; break
+    Write-Host "Failed to extract package. Error: $($_.Exception.Message)"; Pause
 }}
 }
 elseif ($out_file -like "*.exe"){
