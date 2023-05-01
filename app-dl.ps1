750152a562184d012fbcdd87b4476c5b7b87a29b
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-01 13:20:15 +0200

    Added the open option before downloading
    The program will recognise its extension act depending on it

--- app-dl.ps1
+++ app-dl.ps1
@@ -11,75 +11,111 @@ foreach ($i in 0..($nameArray.Count - 1)) {
   $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn }
 }
 
-Select-App
-$pkg_n = [int]($pkg -replace "\.")
+  Clear-Host
+  Write-Main 'Available apps'
+  foreach ($i in 0..($filteredApps.Count - 1)) {
+    $app = $filteredApps[$i]
+    $n = $i + 1
+    Write-Point "$n. $($app.Name)"
+  }
+  Write-Host "`nType a dot before the number to display all the program properties, for example: '.1'"
+  $pkg = Read-Host "`nWrite the number of the app you want to get"
 
 # Assign the corresponding variables to the selected app
-$program = $filteredApps[$pkg_n - 1].Name
-$exe = $filteredApps[$pkg_n - 1].Exe
-$folder = $filteredApps[$pkg_n - 1].folder
-$url = $filteredApps[$pkg_n - 1].URL
-$cmd = $filteredApps[$pkg_n - 1].Cmd
-$cmd_syn = $filteredApps[$pkg_n - 1].Cmd_syn
+$pkg_n = [int]($pkg -replace "\.")
+$selectedApp = $filteredApps[$pkg_n - 1]
+$program = $selectedApp.Name
+$exe = $selectedApp.Exe
+$syn = $selectedApp.Syn
+$folder = $selectedApp.folder
+$url = $selectedApp.URL
+$cmd = $selectedApp.Cmd
+$cmd_syn = $selectedApp.Cmd_syn
 $o = Split-Path $url -Leaf
 
-App-Loop
+
+Write-Main "$program selected"
+if ($pkg -like ".*") {
+  $response = Invoke-WebRequest -Uri $url -Method Head
+  $size = Read-FileSize ([long]$response.Headers.'Content-Length'[0])
+  
+  Write-Point "$program is $syn"
+  Write-Point "Size: $size"
+  if ($exe) { Write-Point "Executable: $exe" }
+  if ($cmd_syn) { Write-Point $cmd_syn }
+  if ($cmd) { Write-Point "Parameters are: $cmd)" }
+  Pause
+  Clear-Host
+  Write-Main 'Available apps'
+  foreach ($i in 0..($filteredApps.Count - 1)) {
+    $app = $filteredApps[$i]
+    $n = $i + 1
+    Write-Point "$n. $($app.Name)"
+  }
+  Write-Host "`nType a dot before the number to display all the program properties, for example: '.1'"
+$pkg = Read-Host "`nWrite the number of the app you want to get"
+}
+
 Write-Main "$program selected"
 
-Select-Path
 Clear-Host
 Write-Main "Selected path: $p"
 
-#Checks if the program is installed or uncompressed in the selected folder
+
 if (Test-Path "$p\$o") { Revoke-Path }
 if (Test-Path "$p\$program\$folder\$exe") { Revoke-Path }
 
 Write-Main "App to download: $program..."
-$d = Read-Host 'Confirmation (press enter or any key to go to the (R)estart menu)'
-if ($d -eq 'R' -or $d -eq 'r') { Restart-Menu }
+Write-Secondary "Do you want to open when finished? (y/n)"
+$open = Read-Host
+do {$open = $false} while (!($open -eq 'y' -or $open -eq 'Y'))
+if ($open -eq 'y' -or $open -eq 'Y') {$open = $true}
+$dl = Read-Host 'Confirmation (press enter or any key to go to the (R)estart menu)'
+if ($dl -eq 'R' -or $dl -eq 'r') { Restart-Menu }
 
 try {
 Invoke-WebRequest -URI $url -OutFile "$p\$o"
   Write-Secondary 'File downloaded successfully'
 }
-catch {
-  Write-Warning "Failed to download package. Error: $($_.Exception.Message)"
-}
+catch {Write-Warning "Failed to download package. Error: $($_.Exception.Message)"}
 
-# Checks the package extension for extracting or installing it
-if ($o -like "*.zip") {
-  $e = Read-Host 'Do you want to unzip the package?(y/n)'
-  if ($e -eq 'y' -or 'Y') {
-    try {
-      Expand-Archive -Path "$p\$o" -DestinationPath "$p\$program" -Force
-      Write-Main 'Package succesfully extracted...'
+if ($open = $true) {
+  Write-Main "Launching $program..."
+  if ($o -like "*.zip") {
+    if (Test-Path -eq "$p\$o") {
+      Write-Main 'Zip file detected'
+      Write-Secondary "$program is saved as a zip file, so uncompressing..."
+      Start-Sleep -Milliseconds 200
+      try {
+        Expand-Archive -Path "$p\$o" -DestinationPath "$p\$program" -Force
+        Write-Main 'Package succesfully extracted...'
+      }
+      catch { Write-Warning "Failed to extract package. Error: $($_.Exception.Message)"; Pause }
+      Start-Sleep -Milliseconds 500
+      Exit
     }
-    catch { Write-Warning "Failed to extract package. Error: $($_.Exception.Message)"; Pause }
+    elseif (Test-Path -eq "$p\$program\$folder") {
+      Start-Process -FilePath "$p\$program\$folder\$exe"
+    }
   }
-  if ($e -ne 'y' -or $e -ne 'Y') {
-    Write-Main 'Leaving session...'
-    Start-Sleep 1
-    Exit
-  }
-
-  $open = Read-Host 'Open the app?(y/n)'
-  if ($open -eq 'y' -or $open -eq 'Y') { Start-Process -FilePath "$p\$program\$folder\$exe" }
-}
-
-
-if ($o -like "*.exe") {
-  if ($cmd) {
-    Write-Host "There is a preset for running $program $($cmd_syn). Do you want to do it (if not, it will just open it as normal)? (y/n)"
-    $runcmd = Read-Host
-    if ($runcmd -eq 'y' -or $runcmd -eq 'Y') {
-      Clear-Host
-      Write-Main "Running $program $($cmd_syn)"
-      Start-Process -FilePath "$p\$o" -ArgumentList $($cmd)
+  if ($o -like "*.exe") {
+    if ($null -ne $cmd) {
+      Write-Host "There is a preset for running $program $($cmd_syn). Do you want to do it (if not, it will just launch it as normal)? (y/n)"
+      $runcmd = Read-Host
+      if ($runcmd -eq 'y' -or $runcmd -eq 'Y') {
+        Clear-Host
+        Write-Main "Running $program $($cmd_syn)"
+        Start-Process -FilePath "$p\$o" -ArgumentList $($cmd)
+        Start-Sleep -Milliseconds 200
+        Exit
+      }
     }
     if ($runcmd -ne 'y' -or $runcmd -ne 'Y') {
       Clear-Host
       Write-Main "Running $program directly"
       Start-Process -FilePath "$p\$o"
+      Start-Sleep -Milliseconds 200
+      Exit
     }
   }
 }
