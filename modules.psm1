c766e7401fee5f4bb1b193d97dd3457f0b2fa0d9
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-02 18:26:09 +0200

    Reset variables
    This is just for the program not to drag any variable from other app selected before (working)

--- modules.psm1
+++ modules.psm1
@@ -45,6 +45,7 @@ function Show-Details {
   if ($cmd_syn) { Write-Point $cmd_syn }
   if ($cmd) { Write-Point "Parameters are: $cmd)" }
   Pause
+  $pkg,$pkg_n,$program,$url,$size,$syn,$exe,$cmd_syn,$cmd = $null # Discards the obtained values for that the user can select them as many times as wanted
 }
 function Show-Paths {
   Write-Point '1. Saves it inside of Desktop'
