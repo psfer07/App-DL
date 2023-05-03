b0d1e5da71e4b6238b39549211a4076ed5d545b7
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-03 16:38:18 +0200

    Removed the necesity of typing a period for listing details

--- app-dl.ps1
+++ app-dl.ps1
@@ -11,7 +11,6 @@ foreach ($i in 0..($nameArray.Count - 1)) {
   $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn }
 }
 
-
 Clear-Host
 Select-App
 $pkg = Read-Host "`nWrite the number of the app you want to get"
@@ -28,14 +27,9 @@ $cmd = $n.Cmd
 $cmd_syn = $n.Cmd_syn
 $o = Split-Path $url -Leaf
 
-
-if ($pkg -match '.') {
-  Clear-Host
-  Write-Main "$program selected"
-  Show-Details
-}
-
-
+Clear-Host
+Write-Main "$program selected"
+Show-Details
 Show-Paths
 [string]$p = Read-Host "`nChoose a number"
 switch ($p) {
