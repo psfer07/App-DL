a685df68523dd33a384a6a261d94eee61e8ae7ba
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-03 16:46:23 +0200

    Removed Exit from Restart

--- modules.psm1
+++ modules.psm1
@@ -130,6 +130,4 @@ function Open-File {
 function Restart {
   $pkg,$pkg_n,$program,$url,$size,$syn,$exe,$cmd_syn,$cmd,$o,$p = $null # Resets variables
   powershell.exe -command "Invoke-RestMethod "https://raw.githubusercontent.com/psfer07/App-DL/$branch/app-dl.ps1" | Invoke-Expression"
-  Start-Sleep 1
-  Exit
 }
