d05120aecda9a02a40d2b3c09f94e2fe3ff828ae
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-06 13:28:06 +0200

    Finally fixed Read-FileSize function
    As always, to switch off and on again is the best solution

--- modules.psm1
+++ modules.psm1
@@ -28,13 +28,18 @@ function Select-App {
     Write-Point "$n. $($app.Name)"
   }
 }
+function Read-FileSize {
+  Param ([int]$length)
+  if ($length -gt 1GB) { [string]::Format("{0:0.00} GB", $length / 1GB) }
+  elseIf ($length -gt 1MB) { [string]::Format("{0:0.00} MB", $length / 1MB) }
+  elseIf ($length -gt 1KB) { [string]::Format("{0:0.00} kB", $length / 1KB) }
+  elseIf ($length -gt 0) { [string]::Format("{0:0.00} B", $length) }
+}
+
 function Show-Details {
   $request = Invoke-WebRequest $url -Method Head
-  $size = [int]$request.Headers['Content-Length']
-  if ($size -gt 1GB) { [string]::Format("{0:0.00} GB", $size / 1GB) }
-  elseIf ($size -gt 1MB) { [string]::Format("{0:0.00} MB", $size / 1MB) }
-  elseIf ($size -gt 1KB) { [string]::Format("{0:0.00} kB", $size / 1KB) }
-  elseIf ($size -gt 0) { [string]::Format("{0:0.00} B", $size) }
+  $length = [int]$request.Headers['Content-Length']
+  $size = Read-FileSize $length
   Write-Main "$program selected"
   Write-Point "$program is $syn"
   Write-Point "Size: $size"
