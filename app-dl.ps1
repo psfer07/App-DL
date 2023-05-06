7c0cffd8930dbb832a174ee1c2a740513083fc48
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-06 14:45:23 +0200

    New v1.2 update

--- app-dl.ps1
+++ app-dl.ps1
@@ -1,147 +1,74 @@
-$branch = "main"
+# Bypasses any execution policy
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
-Clear-Host
-function Write-Main($Text) {
-  $border = "============================================"
-  Write-Host "`n`n<$border>" -ForegroundColor Blue
-  Write-Host "   $Text" -ForegroundColor White
-  Write-Host "<$border>" -ForegroundColor Blue
-}
-function Write-Secondary($Text) {
-  Write-Host "`n<==========[" -NoNewline -ForegroundColor Green
-  Write-Host " $Text " -NoNewline -ForegroundColor White
-  Write-Host "]==========>`n" -ForegroundColor Green
-}
-function Write-Point($Text) {
-  Write-Host "==[ " -NoNewline -ForegroundColor Green
-  Write-Host "$Text" -ForegroundColor White
-}
-function Write-Warning($Text) {
-  $loop = $Text.Length
-  $center = for ($i = 1; $i -le $loop; $i++){ ("=") }
+# Removes last possible modules.psm1 from the computer
+Remove-Item "$Env:TEMP\modules.psm1" -Force -ErrorAction SilentlyContinue
 
-  Write-Host "`n<$center>" -ForegroundColor Red
-  Write-Host "   $Text" -ForegroundColor White
-  Write-Host "<$center>" -ForegroundColor Red
-}
-function Use-Path{
-    Clear-Host
-    Write-Warning "It seems that $program is currently allocated in this path"
-    $restart = Read-Host "Write 'r' to restart the app and start again, 'o' to open the existing app or 'e' to exiting"
-    switch ($restart) {
-      "r"   { Start-Process powershell.exe "-File `"$PSCommandPath`""; Start-Sleep -Milliseconds 200; exit }
-      "o"   { Write-Main "Opening $program..."; if ($out_file -like "*.zip"){ if (Test-Path -eq "$path\$out_file"){ Write-Main "Zip file detected"; Write-Secondary "$program is saved as a zip file, so uncompressing..."; Start-Sleep -Milliseconds 200; Expand-Archive -Path "$path\$out_file" -DestinationPath "$path\$program" -Force; Write-Main "Package succesfully extracted..."; Start-Sleep -Milliseconds 500; exit }elseif (Test-Path -eq "$path\$program\$folder"){Start-Process -FilePath "$path\$program\$folder\$exe"}}; if ($out_file -like "*.exe"){if ($null -ne $cmd) {Write-Host "There is a preset for running $program $($cmd_syn). Do you want to do it (if not, it will just open it as normal)? (y/n)"; $runcmd = Read-Host; if ($runcmd -eq 'y' -or $runcmd -eq 'Y'){; Clear-Host; Write-Main "Running $program $($cmd_syn)"; Start-Process -FilePath "$path\$out_file" -ArgumentList $($cmd); Start-Sleep -Milliseconds 200; exit}}if ($runcmd -eq 'n' -or $runcmd -eq 'N'){Clear-Host; Write-Main "Running $program directly"; Start-Process -FilePath "$path\$out_file"; Start-Sleep -Milliseconds 200; exit}}}
-      "e"   { Write-Main "Closing this terminal..."; Start-Sleep -Milliseconds 500; exit }
-      default { Write-Warning "Non-valid character, exiting..."; Start-Sleep -Milliseconds 500; exit }
-    }
-}
-
-#Initialize variables
+# Imports variables
+[string]$branch = 'main'
+[string]$module = "$Env:TEMP\modules.psm1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/psfer07/App-DL/$branch/modules.psm1" -OutFile $module
+Import-Module $module -DisableNameChecking
 $json = Invoke-RestMethod "https://raw.githubusercontent.com/psfer07/App-DL/$branch/apps.json"
 $nameArray = $json.psobject.Properties.Name
-$propMapping = @{}
 $filteredApps = @()
 
-Clear-Host
-
-#Assigns the JSON's properties into Powershell objects
+# Sets the JSON data into Powershell variables
 foreach ($i in 0..($nameArray.Count - 1)) {
-  $name = $nameArray[$i]
-  $app = $json.$name
-  $folder = $app.folder
-  $url = $app.URL
-  $exe = $app.exe
-  $size = $app.size
-  $cmd = $app.cmd
-  $syn = $app.syn
-  $cmd_syn = $app.cmd_syn
-  $propMapping.Add($name, $url)
-  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Cmd = $cmd; Syn = $syn; Cmd_syn = $cmd_syn}
+  $name = $nameArray[$i]; $app = $json.$name; $folder = $app.folder; $url = $app.URL; $exe = $app.exe; $syn = $app.syn; $cmd = $app.cmd; $cmd_syn = $app.cmd_syn
+  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn }
 }
 
-#List every app valid in the JSON file
+# Lists every single app in the JSON
 Clear-Host
-Write-Main "Available apps"
-foreach ($i in 0..($filteredApps.Count - 1)) {
-  $app = $filteredApps[$i]
-  $n = $i + 1
-  Write-Main "$n. $($app.Name) - Size: $($app.Size)"
-  Write-Point $app.syn
-}
-$pkg_n = Read-Host `n"Write the number of the app you want to get"
+Select-App
+$pkg = Read-Host "`nWrite the number of the app you want to get"
 
-#Assign the corresponding variables to the selected app
-$program =    $filteredApps[$pkg_n - 1].Name
-$exe =        $filteredApps[$pkg_n - 1].Exe
-$folder =     $filteredApps[$pkg_n - 1].folder
-$url =        $filteredApps[$pkg_n - 1].URL
-$cmd_syn =    $filteredApps[$pkg_n - 1].Cmd_syn
-$cmd =        $filteredApps[$pkg_n - 1].Cmd
-$out_file =   (Split-Path $url -Leaf) -split "/" | Select-Object -Last 1
-Clear-Host
+# Assign the corresponding variables to the selected app
+$pkg_n = [int]($pkg -replace "\."); $n = $filteredApps[$pkg_n - 1]; $program = $n.Name; $exe = $n.Exe; $syn = $n.Syn; $folder = $n.folder; $url = $n.URL; $cmd = $n.Cmd; $cmd_syn = $n.Cmd_syn; $o = Split-Path $url -Leaf
+
 Write-Main "$program selected"
+Start-Sleep -Milliseconds 2500
+Clear-Host
+Show-Details
 
-# Prints out all the aviable paths to save the package
-Write-Point "1. Saves it inside of Desktop"
-Write-Point "2. Saves it inside of Documents"
-Write-Point "3. Saves it inside of Downloads"
-Write-Point "4. Save it inside of C:"
-Write-Point "5. Saves it inside of Program Files"
-Write-Point "6. Save it inside of the user profile`n"
-Write-Point "0. Goes back to change the app"
-$path = Read-Host "`nChoose a number"
-
-switch ($path) {
-  0         { Start-Process powershell.exe "-WindowStyle Maximized -File `"$PSCommandPath`""; Start-Sleep -Milliseconds 500; exit }
-  1         { $path = "$Env:USERPROFILE\Desktop"; break }
-  2         { $path = "$Env:USERPROFILE\Documents"; break }
-  3         { $path = "$Env:USERPROFILE\Downloads"; break }
-  4         { $path = $Env:SystemDrive; break }
-  5         { $path = $Env:ProgramFiles; break }
-  6         { $path = $Env:HOMEPATH; break }
-  default   { Write-Host "Invalid input. Using default path: $Env:USERPROFILE"; $path = $Env:USERPROFILE }
-}
-Write-Main "Selected path: $path"
-
-#Checks if the program is installed or uncompressed in the selected folder
-if (Test-Path "$path\$out_file") {Use-Path}
-
-# Downloads the app package
-Write-Main "App to download: $program..."; Pause
-Invoke-WebRequest -URI $url -OutFile "$path\$out_file"
-if ($?) {
-    Write-Secondary "File downloaded successfully"
-} else {
-    Write-Warning "An error occurred while downloading the file: $_.Exception"
-  }
-
-# Extracts or launches the app installer depending on its extension
-if ($out_file -like "*.zip"){$extract = Read-Host "Do you want to unzip the package?(y/n)"
-if ($extract -eq "y" -or $extract -eq "Y"){
-  try {
-    Expand-Archive -Path "$path\$out_file" -DestinationPath "$path\$program" -Force
-    Write-Main "Package succesfully extracted..."
-  }
-  catch { Write-Warning "Failed to extract package. Error: $($_.Exception.Message)"; Pause }
+# Sets all possible paths for downloading the program
+Show-Paths
+[string]$p = Read-Host "`nChoose a number"
+switch ($p) {
+  0 { Restart-App }
+  1 { $p = "$Env:USERPROFILE\Desktop"; break }
+  2 { $p = "$Env:USERPROFILE\Documents"; break }
+  3 { $p = "$Env:USERPROFILE\Downloads"; break }
+  4 { $p = $Env:SystemDrive; break }
+  5 { $p = $Env:ProgramFiles; break }
+  6 { $p = $Env:HOMEPATH; break }
+  'x' { $p = Read-Host 'Set the whole custom path'; break }
+  'X' { $p = Read-Host 'Set the whole custom path'; break }
+  default { Write-Host "Invalid input. Using default path: $Env:USERPROFILE"; $p = $Env:USERPROFILE; break }
 }
 
-$open = Read-Host "Open the app?(y/n)"
-if ($open -eq "y" -or $open -eq "y"){ Start-Process -FilePath "$path\$program\$folder\$exe" }
-}
+Write-Main "Selected path: $p"
 
-if ($out_file -like "*.exe"){
-  if ($null -ne $cmd) {
-    Write-Host "There is a preset for running $program $($cmd_syn). Do you want to do it (if not, it will just open it as normal)? (y/n)"
-    $runcmd = Read-Host
-    if ($runcmd -eq 'y' -or $runcmd -eq 'Y'){
-      Clear-Host
-      Write-Main "Running $program $($cmd_syn)"
-      Start-Process -FilePath "$path\$out_file" -ArgumentList $($cmd)
-    }
-    if ($runcmd -eq 'n' -or $runcmd -eq 'N'){
-      Clear-Host
-      Write-Main "Running $program directly"
-      Start-Process -FilePath "$path\$out_file"
-    }
-  }
-}
+# Checks if the program was allocated there before
+if (Test-Path "$p\$o") { Revoke-Path }
+if (Test-Path "$p\$program\$folder\$exe") { Revoke-Path }
+Start-Sleep -Seconds 1
+
+# Asks the user to open the program after downloading it
+Write-Secondary "Do you want to open it when finished? (y/n)"
+$openAns = Read-Host
+$open = $false
+$openString = $null
+if ($openAns -eq 'y' -or $openAns -eq 'Y') { $open = $true }
+if ($open -eq $true) {$openString = ' and open'}
+
+# Last confirmation
+Write-Main "You are going to download$openString $program"
+$dl = Read-Host 'Confirmation (press any key or go to the (R)estart menu)'
+if ($dl -eq 'R' -or $dl -eq 'r') { Restart-App }
+Clear-Host
+Invoke-RestMethod -Uri $url -OutFile "$p\$o"
+
+if ($?) { Write-Main "File downloaded successfully"} else { Write-Warning "An error occurred while downloading the file" }
+if ($open -eq $true) { Open-File } else {exit}
