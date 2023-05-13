e640ea96a82b6d263a0ff233a9caa87cae969161
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-13 12:50:05 +0200

    Added type property

--- app-dl.ps1
+++ app-dl.ps1
@@ -9,13 +9,21 @@ Remove-Item "$Env:TEMP\modules.psm1" -Force -ErrorAction SilentlyContinue
 # [string]$module = "$Env:TEMP\modules.psm1"
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/psfer07/App-DL/$branch/modules.psm1" -OutFile $module
 # Import-Module $module -DisableNameChecking
+# $json = Invoke-RestMethod "https://raw.githubusercontent.com/psfer07/App-DL/$branch/apps.json"
+$json = Get-Content ".\apps.json" -Raw | ConvertFrom-Json
+Get-Content .\modules.psm1
 Import-Module ".\modules.psm1" -DisableNameChecking
 
 # Sets the JSON data into Powershell variables
-Set-Variables
+$nameArray = $json.psobject.Properties.Name
+$filteredApps = @()
+foreach ($i in 0..($nameArray.Count - 1)) {
+  $name = $nameArray[$i]; $app = $json.$name; $folder = $app.folder; $url = $app.URL; $exe = $app.exe; $syn = $app.syn; $cmd = $app.cmd; $cmd_syn = $app.cmd_syn; $installs = $app.installs; $type = $app.type
+  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn; Installs = $installs; Type = $type }
+}
 
 # Lists every single app in the JSON
-Clear-Host
+#Clear-Host
 Select-App
 $pkg = Read-Host "`nWrite the number of the app you want to get"
 
@@ -24,7 +32,7 @@ $n = $filteredApps[$pkg - 1]; $program = $n.Name; $exe = $n.Exe; $syn = $n.Syn; 
 
 Write-Main "$program selected"
 Start-Sleep -Milliseconds 2500
-Clear-Host
+#Clear-Host
 Show-Details
 
 # Sets all possible paths for downloading the program
@@ -41,15 +49,14 @@ $openAns = Read-Host
 $open = $false
 $openString = $null
 if ($openAns -eq 'y' -or $openAns -eq 'Y') { $open = $true }
-if ($open -eq $true) {$openString = ' and open'}
+if ($open -eq $true) { $openString = ' and open' }
 
 # Last confirmation
 Write-Main "You are going to download$openString $program"
 $dl = Read-Host 'Confirmation (press any key or go to the (R)estart menu)'
 if ($dl -eq 'R' -or $dl -eq 'r') { Restart-App }
 
-Clear-Host
+#Clear-Host
 Invoke-RestMethod -Uri $url -OutFile "$p\$o"
-
-if ($?) { Write-Main "File downloaded successfully"} else { Write-Warning "An error occurred while downloading the file" }
+if ($?) { Write-Main "File downloaded successfully" } else { Write-Warning "An error occurred while downloading the file" }
 if ($open -eq $false) { exit } else { Open-File }
