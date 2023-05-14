c272e7a0df31202bef161b50c6db9f323d48a724
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-05-14 15:23:39 +0200

    Open-File function reverted

--- app-dl.ps1
+++ app-dl.ps1
@@ -122,75 +122,7 @@ while ($wc.IsBusy) {
   Write-Progress -Activity "Downloading $program..." -Status "$downloadedString ($percentage%) complete" -PercentComplete $percentage
 }
 
-if ($open -eq $true) {
-    # Opens the app
-    Write-Main "Launching $program..."
-    if ($o -like "*.zip") {
-      if (Test-Path -Path "$p\$program\$folder") {
-        Write-Main "$program is uncompressed in $p, so opening it directly..."
-        Start-Sleep -Milliseconds 500
-        Start-Process -FilePath "$p\$program\$folder\$exe" -ErrorAction SilentlyContinue
-        Start-Sleep 1
-        Exit
-      }
-      # It uncompresses it and opens the app
-      elseif (Test-Path -LiteralPath "$p\$o") {
-        Write-Main 'Zip file detected'
-        Write-Point "$program is saved as a zip file, so uncompressing..."
-        Start-Sleep -Milliseconds 200
-        Expand-Archive -Literalpath "$p\$o" -DestinationPath "$p\$program" -Force
-        if ($?) {
-          Write-Main 'Package successfully extracted...'
-        }
-        else {
-          Write-Warning "Failed to extract package. Error: $($_.Exception.Message)"
-          Read-Host "Press any key to continue..."
-        }
-        Start-Sleep 2
-        Clear-Host
-        Write-Main "Running $program directly"
-        Start-Sleep -Milliseconds 500
-        Start-Process -FilePath "$p\$program\$folder\$exe" -ErrorAction SilentlyContinue
-        Start-Sleep -Milliseconds 200
-        Exit
-      }
-      break
-    }
-    elseif ($o -like "*.exe") {
-      Write-Main 'Exe file detected'
-    
-      # If there are any recommended parameters for the executable, asks for using them.
-      if ($cmd) {
-        $runcmd = Read-Host "There is a preset for running $program $($cmd_syn). Do you want to do it (if not, it will just launch it as normal)? (y/n)"
-        if ($runcmd -eq 'y' -or $runcmd -eq 'Y') {
-        
-          Write-Main "Running $program $($cmd_syn)"
-          Start-Process -FilePath "$p\$o" -ArgumentList $($cmd) -ErrorAction SilentlyContinue
-          Start-Sleep -Milliseconds 200
-          Exit
-        }
-      }
-      if ($runcmd -ne 'y' -or $runcmd -ne 'Y') {
-      
-        Write-Main "Running $program directly"
-        Start-Process -FilePath "$p\$o" -ErrorAction SilentlyContinue
-        Start-Sleep -Milliseconds 200
-        Exit
-      }
-      break
-    }
-    elseif ($o -like "*.msi") {
-      Write-Main 'Windows installer detected'
-      Write-Main "Installing $program silently"
-      msiexec.exe /i "$p\$o" /passive /norestart
-      Start-Sleep -Milliseconds 200
-      break
-    }
-    elseif ($o -like "*.msix" -or $o -like "*.msixbundle" -or $o -like "*.appx" -or $o -like "*.appxbundle") {
-      Write-Main 'Bundle Microsoft app detected'
-      Add-AppPackage -Path "$p\$o"
-    }
-}
+if ($open -eq $true) { Open-File }
 
 # Continue downloading
 Write-Point 'Do you want to download another app? (y/n)'
