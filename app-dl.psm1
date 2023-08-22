82d997237c1d73ee97cb0d6f260167c8da0f01d7
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-08-22 20:00:50 +0200

    Fixed Open-App
    - Added all necessary parameters to the function; now it's needed.
    - Improved UI
    - Minor label fixes

--- app-dl.psm1
+++ app-dl.psm1
@@ -1,5 +1,5 @@
 function Write-Title {
-  param ([string]$t,[switch]$warn)
+  param ([string]$t, [switch]$warn)
   if ($warn) { $bColor = "Red" } else { $bColor = "Blue" }
   if ($t.Length % 2 -ne 0, 1) { [string]$extra = 'o' }
   $b = "o" * (4 + $t.Length)
@@ -10,7 +10,7 @@ function Write-Title {
   Write-Host "ooo$b$extra" -ForegroundColor Dark$bColor
 }
 function Write-Subtitle {
-  param ([string]$t,[int]$pad = 40)
+  param ([string]$t, [int]$pad = 40)
   if ($t.Length % 2 -ne 0) { [string]$extra = 'o' }
   $b = "o" * (($pad - $t.Length - 3) / 2)
   Write-Host "`n<$b " -NoNewline -ForegroundColor DarkBlue
@@ -18,7 +18,7 @@ function Write-Subtitle {
   Write-Host " $b$extra>" -ForegroundColor DarkBlue
 }
 function Write-Point {
-  param([string]$t,[switch]$NoNewLine = $false)
+  param([string]$t, [switch]$NoNewLine = $false)
   if ($NoNewLine) {
     Write-Host '==> ' -NoNewline -ForegroundColor Magenta
     Write-Host "$t" -NoNewline -ForegroundColor White
@@ -38,20 +38,52 @@ function Get-AppSize($size) {
 }
 function Revoke-Path {
   Write-Title -warn "It seems that $app is currently allocated in this path"
-  do { $reset = Read-Host "You can (r)estart, (o)pen, or (e)xit the app" } while ($reset -ne 'r' -and $reset -ne 'o' -and $reset -ne 'e')
+  do {
+    Write-Host
+    Write-Point  -NoNewLine
+    $reset = Read-Host 'You can (r)estart, (o)pen, or (e)xit the app' 
+  } while ($reset -ne 'r' -and $reset -ne 'o' -and $reset -ne 'e')
 
   switch ($reset) {
     'r' { Start-Main }
-    'o' { Open-App $o }
+    'o' { Open-App $p $o $app $folder $exe $cmd $cmd_syn $portapps }
     'e' { Write-Title 'Closing this terminal...'; Start-Sleep -Milliseconds 500; exit }
   }
 }
 function Open-App {
-  param ([Parameter(Mandatory)][string]$o)
+  param (
+    [Parameter(Position = 0)]
+    [string]$p,
+
+    [Parameter(Position = 1)]
+    [string]$o,
+
+    [Parameter(Position = 2)]
+    [string]$app,
+
+    [Parameter(Position = 3)]
+    [string]$folder,
+
+    [Parameter(Position = 4)]
+    [string]$exe,
+
+    [Parameter(Position = 5)]
+    [string]$cmd,
+
+    [Parameter(Position = 6)]
+    [string]$cmd_syn,
+
+    [Parameter(Position = 7)]
+    [string]$portapps
+  )
   function Open-Extracted {
     if ($cmd) {
       Write-Point "There is a preset for running $app $($cmd_syn). Launch it with presets?" 
-      do { $runcmd = Read-Host '==> (y/n)' } while ($runcmd -ne 'y' -and $runcmd -ne 'n')
+      do {
+        Write-Host
+        Write-Point  -NoNewLine
+        $runcmd = Read-Host 'y/n' 
+      } while ($runcmd -ne 'y' -and $runcmd -ne 'n')
       if ($runcmd -eq 'n') {
         Write-Title "Running $app..."
         Start-Process -FilePath "$p\$app\$folder\$exe" -ErrorAction SilentlyContinue
@@ -68,14 +100,13 @@ function Open-App {
   }
   switch -Wildcard ($o) {
     "*.zip" {
-      Write-Title "Zip file detected"
+      Write-Title 'Zip file detected'
       if (Test-Path -Path "$p\$app\$folder") {
         Write-Title "$app is Available in $path, so opening..."
         Open-Extracted
       }
       # Expand and open the app
       elseif (Test-Path -LiteralPath "$p\$o") {
-        Write-Title 'Zip file detected'
         Write-Point "$app is saved as a zip file, so uncompressing..."
         Expand-Archive -Literalpath "$p\$o" -DestinationPath "$p\$app" -Force
         if ($?) { Write-Title 'Package successfully extracted...' }
@@ -83,28 +114,23 @@ function Open-App {
           Write-Warning "Failed to extract package. Error: $($_.Exception.Message)"
           Read-Host "Press any key to continue..."
         }
-        Write-Subtitle "Running $app directly"
+        Write-Subtitle "Launching $app..."
         Open-Extracted
        
       }
     }
     "*.7z" {
-      # $7z_libs = '7z.exe', '7z.dll'
-      # foreach ($7z_lib in $7z_libs) { $wc.DownloadFile("https://raw.githubusercontent.com/psfer07/App-DL/$branch/7z/$7z_lib", "$Env:TEMP\$7z_lib") }
-      # $wc.Dispose()
-      # $7z = "$assets\7z.exe"
-      $7z = '.\7z\7z.exe'
+      $7z_libs = '7z.exe', '7z.dll'
+      foreach ($7z_lib in $7z_libs) { $wc.DownloadFile("https://raw.githubusercontent.com/psfer07/App-DL/$branch/7z/$7z_lib", "$Env:TEMP\$7z_lib") }
+      $wc.Dispose()
+      $7z = "$assets\7z.exe" <# '.\7z\7z.exe' #>
       if ($portapps) { $exe = "$app".ToLower() + '.exe' }
       Write-Title '7z file detected'
       if (Test-Path -Path "$p\$app\$folder") {
-        Clear-Host
         Write-Title "$app is available in $path, so opening..."
         Open-Extracted
-      
       }
-      # Expand and open the app
       elseif (Test-Path -Path "$p\$o") {
-        Write-Title '7z file detected'
         Write-Point "$app is saved as a 7z file, so uncompressing..."
         Start-Process $7z -ArgumentList "x `"$p\$o`" -o`"$p\$app`"" -Wait -NoNewWindow
         if ($?) { Write-Title 'Package successfully extracted...' }
@@ -118,26 +144,17 @@ function Open-App {
     }
     "*.exe" {
       Write-Title 'Exe file detected'
-
-      if ($cmd) { Write-Point "There is a preset for running $app $($cmd_syn). Launch it with that configuration?" }
-      do { $runcmd = Read-Host '==> (y/n)' } while ($runcmd -ne 'y' -and $runcmd -ne 'n')
-      if ($runcmd -eq 'n') {
-        Write-Title "Running $app directly"
-        Start-Process -FilePath "$p\$o" -ErrorAction SilentlyContinue
-        Start-Sleep -Milliseconds 200
-      }
-      else {    
-        Write-Title "Running $app $($cmd_syn)"
-        Write-Title "Wait until the installation finishes, be patient"
-        Start-Process -FilePath "$p\$o" -ArgumentList $($cmd) -ErrorAction SilentlyContinue
-        Start-Sleep -Milliseconds 200
-      }
+      Open-Extracted
     }
     "*.msi" {
       Write-Title 'Microsoft installer detected'
       Write-Title "Installing $app automatically"
       Write-Point 'Do you want to launch it after installation?'
-      do { $openInst = Read-Host '--> (y/n)' } while ($openInst -ne 'y' -and $openInst -ne 'n')
+      do {
+        Write-Host
+        Write-Point  -NoNewLine
+        $openInst = Read-Host '--> (y/n)' 
+      } while ($openInst -ne 'y' -and $openInst -ne 'n')
       Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$p\$o`" /passive /promptrestart" -Wait
       Write-Title "$app successfully installed"
       if ($openInst -eq 'y') {
@@ -167,7 +184,7 @@ function Open-App {
     }
     default {
       Write-Title 'Bundle Microsoft app detected'
-      Add-AppPackage -Path "$p\$o" -ForceApplicationShutdown -SkipLicense -Confirm:$false
+      Add-AppPackage -Path "$p\$o" -ForceApplicationShutdown -Confirm:$false
     }
   }
 }
