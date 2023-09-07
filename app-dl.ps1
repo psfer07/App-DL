4d458a0b02adf82fe2b410a688944f7609d31bbb
psfer07 <102543836+psfer07@users.noreply.github.com>
2023-09-07 14:18:38 +0100

    The big update
    - Added parameters -app, -path, -portable and -open for automation (details explained in README)
    - Changed UI background to black
    - Changed window title to App-DL
    - Created the Start-Main function, to recall it instead of redownloading the whole script again
    - Created AppDL folder in the user's temporals
    - Optimized download and usage of libraries
    - Added removal of AppDL folder when the script closes, at any point of it by using a try/finally statement
    - Added only accepting valid inputs for the Read-Host inputs
    - Removed preassignation of every property in apps
    - Added support for a existing app parameter
    - Added category selection
    - Added the displaying of [PORTABLE] or [INSTALLS] depending on what is available to the app
    - Improved app properties assignation
    - Added support for a existing portable parameter
    - Use of the Test-Connection cmdlet instead of working with $request. Now it makes the request only when the host is reachable
    - Improved details displaying by removing unnecessary elements such as the folder or executable
    - Added support for a existing path parameter
    - Used a loop to display available paths to reduce redundancies
    - Added support for handling unknown paths
    - Optimized checking if the app exists in path
    - Added support for a existing open parameter
    - Added skipping some information displays such as confirmation of details prompting when all parameters are provided and valid
    - Optimized progress bar displaying
    - Improved System.Net.WebClient integration
    - Minor label changes
    Shorter commits from now 💀

--- app-dl.ps1
+++ app-dl.ps1
@@ -1,130 +1,295 @@
-# Bypasses any execution policy
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+param (
+  [Parameter(Position = 0)]
+  [string]$app,
+  [Parameter(Position = 1)]
+  [string]$path,
+  [string]$portable = $null,
+  [string]$open = $null
+)
+try {
+  # Bypass any execution policy
+  Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+  $Host.UI.RawUI.BackGroundColor = 'Black'
+  $Host.UI.RawUI.WindowTitle = 'App-DL'
+  Clear-Host
+  function Start-Main {
+    param (
+      [Parameter(Position = 0)]
+      [string]$app,
+      [Parameter(Position = 1)]
+      [string]$path,
+      [string]$portable = $null,
+      [string]$open = $null
+    )
+    $wc = New-Object System.Net.WebClient
+    $branch = 'dev'
+    $tempFolder = Join-Path $Env:TEMP 'AppDL'
+    $assets = Join-Path $tempFolder 'assets'
+    if (!(Test-Path $assets -PathType Container)) { New-Item -ItemType Directory -Path $assets -Force | Out-Null }
+    foreach ($lib in 'apps.json', 'app-dl.psm1') { $wc.DownloadFile("https://raw.githubusercontent.com/psfer07/App-DL/$branch/$lib", "$assets\$lib") }
+    Import-Module <# "$assets\app-dl.psm1" #> '.\app-dl.psm1' -DisableNameChecking -Force
+    $json = Get-Content <# "$assets\apps.json" #> '.\apps.json' -Raw | ConvertFrom-Json
 
-# Removes last possible modules.psm1 from the computer
-Remove-Item "$Env:TEMP\modules.psm1" -Force -ErrorAction SilentlyContinue
+    if ($app) {
+      # Extracts the exact name from the JSON using the app parameter
+      foreach ($category in $json.PSobject.Properties) {
+        $appsInCategory = $category.Value.PSobject.Properties.Name
+        $matchedApp = $appsInCategory | Where-Object { $_ -ieq $app }
+        if ($matchedApp) { $matchedAppKey = $matchedApp; break }
+      }
+      if (-not $matchedAppKey) {
+        Write-Warning "No app recognized, starting App-DL by default"
+        Start-Sleep 1
+        Start-Main
+      }
+      $app = $matchedAppKey
+    }
+    else {
+      do {
+        $categories = $json.psobject.Properties.Name
+        Clear-Host
+        Write-Title 'Available categories' 
+        for ($i = 0; $i -lt $categories.Count; $i++) { Write-Subtitle "$($i+1). $($categories[$i])" }
+        do {
+          Write-Host
+          Write-Point -NoNewLine
+          $categoryN = Read-Host "Write the number of the category you want to see"
+        } while ($categoryN -notmatch '^\d+$' -or !([int]::TryParse($categoryN, [ref]$categoryN)) -or $categoryN -lt 1 -or $categoryN -gt $categories.Count)
+        $category = $categories[$categoryN - 1]
+        Clear-Host
+        $programs = $json.$category.psobject.Properties
+        Write-Title "Available apps in $($category.ToLower())"
+        
+        foreach ($program in $programs) {
+          $name = $program.Name
+          $versions = $program.value.versions
+          $details = $program.value.details
+          $spaces = " " * (30 - $name.Length)
 
-# Imports variables
-[string]$branch = 'dev'
-[string]$mod = "$Env:TEMP\modules.psm1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/psfer07/App-DL/$branch/modules.psm1" -OutFile $mod
-Import-Module $mod -DisableNameChecking -Force
-$json = Invoke-RestMethod "https://raw.githubusercontent.com/psfer07/App-DL/$branch/apps.json"
-# $json = Get-Content ".\apps.json" -Raw | ConvertFrom-Json
-# Import-Module ".\modules.psm1" -DisableNameChecking -Force
+          if ($versions -match 'PI') {
+            Write-Point "$name" -NoNewline
+            Write-Host "$spaces[PORTABLE]" -NoNewLine -ForegroundColor Green
+            Write-Host ' & ' -NoNewLine
+            Write-Host '[INSTALLS] ' -ForegroundColor Red
+          }
+          elseif ($versions -match 'P') {
+            Write-Point "$name" -NoNewline
+            Write-Host "$spaces[PORTABLE]              " -ForegroundColor Green
+          }
+          elseif ($versions -match 'I') {
+            Write-Point "$name" -NoNewline
+            Write-Host "$spaces             [INSTALLS]" -ForegroundColor Red
+          }
+        }
 
-# Sets the JSON data into Powershell variables
-$nameArray = $json.psobject.Properties.Name
-$filteredApps = @()
-foreach ($i in 0..($nameArray.Count - 1)) {
-  $name = $nameArray[$i]; $app = $json.$name; $folder = $app.folder; $url = $app.URL; $exe = $app.exe; $syn = $app.syn; $cmd = $app.cmd; $cmd_syn = $app.cmd_syn; $type = $app.type
-  $filteredApps += [PsCustomObject]@{Index = $i; Name = $name; Folder = $folder; URL = $url; Exe = $exe; Size = $size; Syn = $syn; Cmd = $cmd; Cmd_syn = $cmd_syn; Type = $type }
+        Write-Subtitle "0. Return to categories"
+        do {
+          Write-Host
+          Write-Point -NoNewLine
+          $appN = Read-Host "Write the number of the app you want to get"
+        } while ($appN -notmatch '^\d+$' -or !([int]::TryParse($appN, [ref]$appN)) -or $appN -lt 0 -or $appN -gt $($programs.Name).Count)
+      } while ($null -eq $appN -or $appN -eq 0)
+      $app = $programs.Name[$appN - 1]
+    }
+    $appCategory = $json.PSobject.Properties | Where-Object { $_.Value.PSObject.Properties.Name -ieq $app } | Select-Object -ExpandProperty Name
+    $appProperties = $json.$appCategory.$app
+    switch ($portable) {
+      y { $ver = 1 }
+      n { $ver = 0 }
+      default {
+        if ($portable) {
+          Write-Warning "Non-valid entry for portable: $portable"
+          Start-Sleep 1
+        }
+        if ($appProperties.versions -match 'PI') {
+          Write-Title "$app also has a portable version"
+          Write-Point "Write (Y)es or (N)o to select the portable version`n"
+          do {
+            Write-Host
+            Write-Point -NoNewLine
+            $selectPortable = Read-Host 'y/n' 
+          } while ($selectPortable -ne 'y' -and $selectPortable -ne 'n')
+          switch ($selectPortable) {
+            y { $ver = 1 }
+            n { $ver = 0 }
+          }
+        }
+      }
+    }
+    if ($appProperties.versions -cnotmatch 'PI' -and $portable) {
+      Write-Warning "$app does not support an alternative version"
+      Start-Sleep -Milliseconds 1500
+    }
+    
+    Write-Title 'Importing data...'
+    $properties = 'app', 'url', 'folder', 'details', 'versions', 'exe', 'size', 'syn', 'cmd', 'cmd_syn'
+    foreach ($property in $properties) {
+      if ($appProperties.$property -is [array]) { $value = $appProperties.$property[$ver] } else { $value = $appProperties.$property }
+      New-Variable -Name $property -Value $value -ErrorAction SilentlyContinue
+    }
+    $uri = [System.Uri]$url
+    $reachable = Test-Connection -ComputerName $uri.Host -Count 1 -ErrorAction SilentlyContinue
+
+    if ($reachable) {
+      $request = Invoke-WebRequest $uri -Method Head -UseBasicParsing
+      $length = [int]$request.Headers['Content-Length']
+    }
+    else {
+      Write-Warning "Host is not reachable. Please check your network connection or contact the developer."
+      Start-Sleep 4
+      Clear-Host
+      Start-Main
+    }
+    $filesize = Get-AppSize $length
+    Clear-Host
+    if (!($matchedAppKey -and $portable -and $path -and $open)) {
+      if ($ver -eq 1) { Write-Title "$app (portable version)" } else { Write-Title $app }
+      if ($size) {
+        if ($ver -eq 0) { $pkgOinst = 'Installer' } else { $pkgOinst = 'Package' }
+        Write-Point "$pkgOinst size: $filesize`n"
+        Write-Point "Total app size: $size`n"
+      }
+      else { Write-Point "Total app size: $filesize`n" }
+      Write-Point "This program is $syn`n"
+      if ($cmd_syn) { Write-Point "You can open it $cmd_syn`n" }
+    }
+
+    $paths = @{
+      'desktop'       = "$Env:USERPROFILE\Desktop" 
+      'documents'     = "$Env:USERPROFILE\Documents"
+      'downloads'     = "$Env:USERPROFILE\Downloads"
+      'c'             = $Env:SystemDrive
+      'programfiles'  = $Env:ProgramFiles
+      'program files' = $Env:ProgramFiles
+      'userprofile'   = $Env:HOMEPATH
+      'user profile'  = $Env:HOMEPATH
+      'appdl'         = "$tempFolder\Downloads"
+    }
+    if ($path) {
+      $inputPath = $path.ToLower()
+      $p = $paths["$inputPath"]
+      if (!(Test-Path $p) -and $path -notlike 'appdl') {
+        do {
+          Write-Title -warn 'The provided path does not exist'
+          Write-Host "Path: $p"
+          Write-Point 'Do you want to (C)reate it now or use (A)nother path?'
+          do {
+            Write-Host
+            Write-Point -NoNewLine
+            $newPath = Read-Host 'Write a letter' 
+          } while ($newPath -ne 'c' -and $newPath -ne 'a')
+          if ($newPath -eq 'c') { New-Item -ItemType Directory -Path $path -Force }
+          elseif ($newPath -eq 'a') { $path = Read-Host 'Write the full path' }
+        } while ((Test-Path $path) -ne $true)
+        $p = $path
+        $path = 'a custom directory'
+      }
+      elseif ($path -like 'appdl') { New-Item -ItemType $p -force | Out-Null }
+      else { $path = $inputPath }
+    }
+    else {
+      Write-Title 'Path selecting'
+      [string]$s = 'Save it'
+      $pathOptions = @("$s in Desktop", "$s in Documents", "$s in Downloads", "$s in C:", "$s in Program Files", "$s in the user profile", "$s in the roaming folder`n", "$s in App-DL's temp folder (opens the program automatically)`n")
+    
+      for ($i = 1; $i -le $pathOptions.Length; $i++) { 
+        Write-Point "$i. $($pathOptions[$i-1])" 
+      }
+    
+      Write-Point 'X. Introduce a custom path'
+      Write-Point "0. Resets the program to select another app`n"
+    
+      do {
+        Write-Host
+        Write-Point -NoNewLine
+        $pathN = Read-Host "Choose an option" 
+      } while ($pathN -notmatch '^\d+$' -or $pathN -lt 0 -or $pathN -gt $pathOptions.Count -and $pathN -ne 'x')
+    
+      switch ($pathN) {
+        0 { Start-Main }
+        1 { $p = "$Env:USERPROFILE\Desktop"; break }
+        2 { $p = "$Env:USERPROFILE\Documents"; break }
+        3 { $p = "$Env:USERPROFILE\Downloads"; break }
+        4 { $p = $Env:SystemDrive; break }
+        5 { $p = $Env:ProgramFiles; break }
+        6 { $p = $Env:HOMEPATH; break }
+        7 { $p = "$Env:APPDATA\roaming"; break }
+        8 { $p = "$tempfolder\downloads"; New-Item -ItemType Container -Path $p -Force | Out-Null; $opens = $true; break }
+        x { $p = Read-Host 'Set the whole custom path'; break }
+      }
+      $matchingPath = $paths.Keys | Where-Object { $paths[$_] -eq $p }
+      if ($matchingPath) { $path = $matchingPath }
+      else {
+        if (!(Test-Path $p)) {
+          do {
+            Write-Title -warn 'The provided path does not exist'
+            Write-Host $path
+            Write-Subtitle 'Do you want to (C)reate it now or use (A)nother path?' -pad 50
+            do {
+              Write-Host
+              Write-Point -NoNewLine
+              $newPath = Read-Host 'Write a letter' 
+            } while ($newPath -ne 'c' -or $newPath -ne 'a')
+            if ($newPath -eq 'c') { New-Item -ItemType Directory -Path $path -Force }
+            elseif ($newPath -eq 'a') { $path = Read-Host 'Write the full path' }
+          } while ((!(Test-Path $path)))
+          $path = 'a custom directory'
+        }
+      }
+    }
+  
+    $o = $uri.Segments[-1]
+    if ((Test-Path "$p\$o") -or (Test-Path "$p\$app\$folder\$exe")) { Revoke-Path -p $p -o $o -app $app -folder $folder -exe $exe -details $details -cmd $cmd -cmd_syn $cmd_syn; break }
+    if (!($matchedAppKey -and $portable -and $path -and $open)) { Write-Subtitle "$app will be saved in $path" -pad 70 }
+  
+    switch ($open) {
+      y { $opens = $true; $openString = ' and open' }
+      n { $opens = $false; $openString = $null }
+      default {
+        if ($open) {
+          Write-Warning "Non-valid entry in open: $open"
+          Start-Sleep 1
+        }
+        Write-Host
+        Write-Point 'Do you want to open the file when downloaded?'
+        do {
+          Write-Host
+          Write-Point -NoNewLine
+          $openAns = Read-Host 'y/n' 
+        } while ( $openAns -ne 'y' -and $openAns -ne 'n' )
+        switch ($openAns) {
+          'y' { $opens = $true }
+          'n' { $opens = $false }
+        }
+      }
+    }
+
+    if (!($matchedAppKey -and $portable -and $path -and $open)) {
+      Write-Title "You are going to download$openString $app"
+      $confirm = Read-Host 'Confirmation press any key or go to the (R)estart menu'
+      if ($confirm -eq 'r') { Start-Main }
+    }
+
+    # Update progress bar
+    $wc.DownloadFileAsync($uri, "$p\$o")
+    while ($wc.IsBusy) {
+      $downloadedOld = (Get-Item "$p\$o").Length
+      Start-Sleep -Milliseconds 250
+      $Downloaded = (Get-Item "$p\$o").Length
+      $MBs = [Math]::Round(($downloaded - $DownloadedOld) * 4) / 1MB
+      $percentage = [Math]::Round(($downloaded / $length) * 100)
+      $downloadedString = "{0:N2} MB / {1:N2} MB at {2:N2} MB/s" -f ($downloaded / 1MB), ($length / 1MB), $MBs
+      Write-Progress -Activity "Downloading $app..." -Status "$downloadedString ($percentage%) complete" -PercentComplete $percentage
+    }
+
+    if ($o -notlike "*.7z") { $wc.Dispose | Out-Null }
+    if ($opens -eq $true) { Clear-Host; Open-App -p $p -o $o -app $app -folder $folder -exe $exe -details $details -cmd $cmd -cmd_syn $cmd_syn }
+    Write-Subtitle 'Continue downloading?'
+    $repeat = Read-Host '==> (y/n)'
+    if ($repeat -eq 'y') { Clear-Host; Start-Main }
+    else { Clear-Host; Exit }
+  
+  }
+  Start-Main -app $app -path $path -portable $portable -open $open 
 }
-
-# Lists every single app in the JSON
-Clear-Host
-Write-Main 'Available apps'
-foreach ($i in 0..($filteredApps.Count - 1)) {
-  $app = $filteredApps[$i]
-  $n = $i + 1
-  $enum = "$n. $($app.Name)"
-  $spaces = " " * (30 - $enum.Length)
-  Write-Point "$enum$spaces | Related to $($app.Type)"
-}
-$pkg = Read-Host "`nWrite the number of the app you want to get"
-
-# Assign the corresponding variables to the selected app
-$n = $filteredApps[$pkg - 1]; $program = $n.Name
-Write-Main "$program selected"
-
-# Assign the left variables for quicker response
-$exe = $n.Exe; $syn = $n.Syn; $folder = $n.folder; $url = $n.URL; $cmd = $n.Cmd; $cmd_syn = $n.Cmd_syn; $type = $n.type
-$o = Split-Path $url -Leaf; $open = $false
-$request = Invoke-WebRequest $url -Method Head
-if ($request.error -eq 404) {
-  Write-Warning "This program is not currently aviable"
-  Write-Host "Contact the developer for him to fix it"
-  Restart-App
-}
-$length = [int]$request.Headers['Content-Length']
-$size = Get-AppSize $length
-
-Start-Sleep -Milliseconds 500
-Clear-Host
-# Display details
-Write-Main "$program selected"
-Write-Point "$program is $syn"
-Write-Point "Size: $size"
-if ($folder) { Write-Point "Saved in: $folder" }
-if ($exe) { Write-Point "Executable: $exe" }
-if ($cmd_syn) { Write-Point $cmd_syn }
-if ($cmd) { Write-Point "Parameters are: $cmd" }
-
-# Sets all possible paths for downloading the program
-Write-Main 'Path selecting'
-Write-Point '1. Saves it inside of Desktop'
-Write-Point '2. Saves it inside of Documents'
-Write-Point '3. Saves it inside of Downloads'
-Write-Point '4. Saves it inside of C:'
-Write-Point '5. Saves it inside of Program Files'
-Write-Point "6. Saves it inside of the user profile`n"
-Write-Point "7. Saves it temporarily (opens it automatically)`n"
-Write-Point 'X. Introduce a custom path'
-Write-Point '0. Resets the program to select another app'
-[string]$p = Read-Host "`nChoose a number"
-switch ($p) {
-  0 { Restart-App }
-  1 { $p = "$Env:USERPROFILE\Desktop"; break }
-  2 { $p = "$Env:USERPROFILE\Documents"; break }
-  3 { $p = "$Env:USERPROFILE\Downloads"; break }
-  4 { $p = $Env:SystemDrive; break }
-  5 { $p = $Env:ProgramFiles; break }
-  6 { $p = $Env:HOMEPATH; break }
-  7 { $p = $Env:TEMP; $open = $true; break }
-  'x' { $p = Read-Host 'Set the whole custom path'; break }
-  default { Write-Host "Invalid input. Using default path: $Env:USERPROFILE"; $p = $Env:USERPROFILE; break }
-}
-
-# Checks if the program was allocated there before
-if (Test-Path "$p\$o") { Revoke-Path; break }
-if (Test-Path "$p\$program\$folder\$exe") { Revoke-Path; break }
-
-Clear-Host
-# Asks the user to open the program after downloading it
-$openString = $null
-if ($open -eq $false) {
-  Write-Main "Selected path: $p"
-  Write-Point "Do you want to open the DOWNLOADED file when finished? (y/n)"
-  $openAns = Read-Host
-  if ($openAns -eq 'y' -or $openAns -eq 'Y') { $open = $true }
-}
-else { $openString = ' and open' }
-
-# Last confirmation
-Write-Main "You are going to download$openString $program"
-$conf = Read-Host 'Confirmation press any key or go to the (R)estart menu)'
-if ($conf -eq 'R' -or $conf -eq 'r') { Restart-App }
-
-# Start download
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFileAsync($url, "$p\$o")
-
-# Update the download progress
-while ($wc.IsBusy) {
-  $downloadedOld = (Get-Item "$p\$o").Length
-  Start-Sleep -Milliseconds 200
-  $Downloaded = (Get-Item "$p\$o").Length
-  $MBs = ($downloaded - $DownloadedOld)*5
-  $MBs = [Math]::Round($MBs) / 1MB
-  $percentage = ($downloaded / $length) * 100
-  $percentage = [Math]::Round($percentage)
-  $downloadedString = "{0:N2} MB / {1:N2} MB at {2:N2} MB/s" -f ($downloaded / 1MB), ($length / 1MB), $MBs
-  Write-Progress -Activity "Downloading $program..." -Status "$downloadedString ($percentage%) complete" -PercentComplete $percentage
-}
-
-if ($open -eq $true) { Clear-Host; Open-File }
-
-# Continue downloading
-Write-Point 'Do you want to download another app? (y/n)'
-$repeat = Read-Host
-if ($repeat -eq 'y' -or $repeat -eq 'Y') { Restart-App }
+finally { Remove-Item "$Env:TEMP\AppDL" -Recurse -Force | Out-Null }
